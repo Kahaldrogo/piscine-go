c6aa403f00ba12a300fe3602c5f4aac8585dee34
Moctar <moctar@localhost.localdomain>
2019-06-09 01:37:11 +0000

    commit

---
+++ printcomb.go
@@ -0,0 +1,16 @@
+package piscine
+
+import "fmt"
+
+func PrintComb(){
+	j:=1
+	k:=2
+	for i := 0; i <= 7; i++ {
+		for j = i + 1; j <= 8; j++ {
+			for k = j + 1; k <= 9; k++ {
+				fmt.Println( i, j, k, ", ")
+
+			}
+		}
+	}
+}
