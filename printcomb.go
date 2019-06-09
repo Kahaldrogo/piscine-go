e8d79c95ff3dfefa275820c7cfa7bc655d55fc25
Moctar <moctar@localhost.localdomain>
2019-06-09 01:39:10 +0000

    commit

--- printcomb.go
+++ printcomb.go
@@ -8,7 +8,7 @@ func PrintComb(){
 	for i := 0; i <= 7; i++ {
 		for j = i + 1; j <= 8; j++ {
 			for k = j + 1; k <= 9; k++ {
-				fmt.Println( i, j, k, ", ")
+				fmt.Println( i,j,k, ",")
 
 			}
 		}
