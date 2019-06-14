5ff9fa0529f3a5b22e6a224dae06658f687e733c
Moctar <moctar@localhost.localdomain>
2019-06-14 10:57:14 +0000

    commit

--- findnextprime.go
+++ findnextprime.go
@@ -2,16 +2,18 @@ package piscine
 
 import "math"
 
-func IsPrime(nb int) bool {
-	for i := 2; i <= int(math.Floor(float64(nb) / 2)); i++ {
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return nb > 1
-}
+
 
 func FindNextPrime(nb int) int {
+	func IsPrime(nb int) bool {
+		for i := 2; i <= int(math.Floor(float64(nb) / 2)); i++ {
+			if nb%i == 0 {
+				return false
+			}
+		}
+		return nb > 1
+	}
+
 	for !IsPrime(nb){
 		nb+=1
 	}
