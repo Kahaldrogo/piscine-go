34f369e76ef71bbcdbd1b103eb595247cd06a6ac
Moctar <moctar@localhost.localdomain>
2019-06-14 10:54:17 +0000

    commit

---
+++ findnextprime.go
@@ -0,0 +1,19 @@
+package piscine
+
+
+
+func IsPrime(nb int) bool {
+	for i := 2; i <= int(math.Floor(float64(nb) / 2)); i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return nb > 1
+}
+
+func FindNextPrime(nb int) int {
+	for !IsPrime(nb){
+		nb+=1
+	}
+	return nb
+}
