4616637cddad373320ba7b165db3644ac96512f7
Moctar <moctar@localhost.localdomain>
2019-06-14 11:10:09 +0000

    commit

--- findnextprime.go
+++ findnextprime.go
@@ -5,16 +5,7 @@ import "math"
 
 
 func FindNextPrime(nb int) int {
-	func IsPrime(nb int) bool {
-		for i := 2; i <= int(math.Floor(float64(nb) / 2)); i++ {
-			if nb%i == 0 {
-				return false
-			}
-		}
-		return nb > 1
-	}
-
-	for !IsPrime(nb){
+      for !IsPrime(){
 		nb+=1
 	}
 	return nb
